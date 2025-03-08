5e956c6baf6e924a36963d21bc066a34d4529546
Ebunoluwanimiii <ebunoluwaoloyede@gmail.com>
2025-03-08 16:10:27 +0000

    modified vpc.tf file

--- create-vpc.tf
+++ create-vpc.tf
@@ -1,7 +1,7 @@
 # Create a new VPC to launch our instances into
 resource "aws_vpc" "custom" {
   cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
+  enable_dns_support1   = true
   enable_dns_hostnames = true
   tags = {
     Name = "custom_vpc"
